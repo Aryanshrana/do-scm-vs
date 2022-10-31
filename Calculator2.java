4391097c2aa25458e83bbf1af89cc52117461905
Aryansh <500093038@stu.upes.ac.in>
2022-10-31 12:04:44 +0530

    addition function added

---
+++ Calculator2.java
@@ -0,0 +1,12 @@
+public class Calculator2 {
+    void addition(int a, int b) {
+        int r = a + b;
+        System.out.println(r);
+    }
+
+    public static void main(String[] args) {
+        Calculator2 bj = new Calculator2();
+        bj.addition(5, 6);
+    }
+
+}
