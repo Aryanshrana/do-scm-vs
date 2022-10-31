bbb16f7df04f6b0988c3ea1d8c94831122c1ea02
Aryansh <500093038@stu.upes.ac.in>
2022-10-31 12:10:12 +0530

    multiplication function added

--- Calculator2.java
+++ Calculator2.java
@@ -9,10 +9,16 @@ public class Calculator2 {
         System.out.println(s);
     }
 
+    void multiplication(int a, int b) {
+        int m = a * b;
+        System.out.println(m);
+    }
+
     public static void main(String[] args) {
         Calculator2 bj = new Calculator2();
         bj.addition(5, 6);
         bj.subtraction(9, 4);
+        bj.multiplication(12, 5);
     }
 
 }
