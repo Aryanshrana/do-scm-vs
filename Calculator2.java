1bb564d628e33043e89b82ea7fbd27f532387762
Aryansh <500093038@stu.upes.ac.in>
2022-10-31 12:07:43 +0530

    subtraction function added

--- Calculator2.java
+++ Calculator2.java
@@ -4,9 +4,15 @@ public class Calculator2 {
         System.out.println(r);
     }
 
+    void subtraction(int a, int b) {
+        int s = a - b;
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
         Calculator2 bj = new Calculator2();
         bj.addition(5, 6);
+        bj.subtraction(9, 4);
     }
 
 }
